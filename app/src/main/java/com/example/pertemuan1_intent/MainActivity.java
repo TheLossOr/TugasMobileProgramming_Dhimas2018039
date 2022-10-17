6097f64f6bbed4dceb9fdd33c8d88685941b7496
TheLossOr <Dhimas.leren77@gmail.com>
2022-10-17 21:02:38 +0700

    Dhimas Setyohadi Pratama - 2018039

--- app/src/main/java/com/example/pertemuan1_intent/MainActivity.java
+++ app/src/main/java/com/example/pertemuan1_intent/MainActivity.java
@@ -18,9 +18,4 @@ public class MainActivity extends AppCompatActivity {
         Intent a = new Intent(MainActivity.this,Biodata.class);
         startActivity(a);
     }
-
-    public void segitiga(View seg){
-        Intent b = new Intent(MainActivity.this,Segitiga.class);
-        startActivity(b);
-    }
 }
